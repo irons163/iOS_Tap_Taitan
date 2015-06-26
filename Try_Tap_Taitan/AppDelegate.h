ee6d2585410bb61efaa60c27d29c96c095b84095
irons163 <irons@ironsdeMacBook-Air.local>
2015-06-26 17:29:17 +0800

    Initial Commit

---
+++ Try_Tap_Taitan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Try_Tap_Taitan
+//
+//  Created by irons on 2015/6/26.
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Try_Tap_Taitan/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Try_Tap_Taitan
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
